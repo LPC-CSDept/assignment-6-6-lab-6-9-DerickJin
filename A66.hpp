f348c9d1625c22aaa7c01866669a6782d093f267
Derick Jin <jinderick5@gmail.com>
2022-10-02 22:21:38 -0700

    a66

--- A66.hpp
+++ A66.hpp
@@ -5,3 +5,29 @@
 using namespace std;
 
 // complete the function maxmin( )
+int maxmin(int a,int b,int c,int& max,int& min)
+{
+    min = a;
+    max = a;
+    int difference;
+    if (min > b)
+    {
+        min = b;
+    }
+    else if (min > c)
+    {
+        min = c;
+    }
+
+    if (max < b)
+    {
+        max = b;
+    }
+    else if (max < c)
+    {
+        max = c;
+    }
+    
+    difference = max-min;
+    return difference;
+}
